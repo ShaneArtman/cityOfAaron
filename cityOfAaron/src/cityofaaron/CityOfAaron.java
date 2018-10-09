ce201ae0e1b9fd80c10e7692533b2b84d86476a6
ShaneArtman <MrShaneArtman@Gmail.com>
2018-10-09 13:40:10 -0400

    Tested and operating as expected.

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -6,18 +6,30 @@
  * All Classes are to be written as Java Beans (Serializable)
  */
 package cityofaaron;
+//import the classes from the model package
+import model.*;
 
-/**
- *
- * @author ShaneArtman
- */
 public class CityOfAaron {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Create an instance of the player and game classes
+        
+        //Player Class instantiated
+        Player myPlayer = new Player();
+        myPlayer.setName("Shane");
+        
+        //Game Class instantiated
+        Game myGame = new Game();
+        myGame.setPlayer(myPlayer);
+        
+        
+        System.out.println("The player's name is: " + myGame.getPlayer().getName() + "\n");
+        System.out.println("One of the team members name is: " + TeamMember.SHANE.getName());
+        System.out.println("And their title is:" + TeamMember.SHANE.getTitle());
+        
     }
     
 }
