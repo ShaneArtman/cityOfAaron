72c9da83cd7f7f5e1404619dad1ec77c114d6c6d
nrham <nrham@NX-T460p>
2018-10-08 18:36:06 -0700

    Working Program

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,14 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor. Test
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ *  Team members: Shane Artman, Nick Hammond, Jonathan Unga
  */
 package cityofaaron;
 
 /**
  *
- * @author artieman1
+ * @author ShaneArtman
  */
 public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
