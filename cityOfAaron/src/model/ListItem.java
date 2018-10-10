cf4cb47a6dca5eeaf042d98958c963a3dab71896
nrham <nrham@NX-T460p>
2018-10-09 22:50:33 -0700

    Merge origin/master
    Conflicts:
    	cityOfAaron/src/cityofaaron/CityOfAaron.java

---
+++ cityOfAaron/src/model/ListItem.java
@@ -0,0 +1,46 @@
+/*
+ * ListItem.java
+ *
+ * The Player Class for the cityOfAaron project
+ * Created in the model package.
+ * 
+ * Created on Oct 8, 2018, 09:00:36 PM
+ * Created by Team
+ *
+ * CIT-260: 08
+ * Fall 2018
+ * Team members: Shane Artman, Nick Hammond, Jonathan Unga
+ *
+ * Purpose: <What is the purpose of the file?>
+ */
+
+package model;
+/*
+ * Implements the serializable interface to write class as a Java Bean
+*/
+import java.io.Serializable;
+
+public class ListItem implements Serializable {
+    
+    private String name;
+    private int number;
+    
+    // public constructor
+    public ListItem(){ }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String _name) {
+        this.name = _name;
+    }
+    
+    public int getNumber(){
+        return this.number;
+    }
+    
+    public void setNumber(int _number) {
+        this.number = _number;
+    }
+}
