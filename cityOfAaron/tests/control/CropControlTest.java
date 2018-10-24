f4596aa022b381f43688c2bf582702d308c90a11
nrham <nrham@NX-T460p>
2018-10-23 18:00:53 -0700

    Test Junit

---
+++ cityOfAaron/tests/control/CropControlTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import model.CropData;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nrham
+ */
+public class CropControlTest {
+    
+    public CropControlTest() {
+    }
+
+    /**
+     * Test of calcLandCost method, of class CropControl.
+     */
+    @Test
+    public void testCalcLandCost() {
+        System.out.println("calcLandCost");
+        int expResult = 0;
+        int result = CropControl.calcLandCost();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sellLand method, of class CropControl.
+     */
+    @Test
+    public void testSellLand() {
+        System.out.println("sellLand");
+        int landPrice = 10;
+        int acresToSell = 20;
+        CropData cropData = null;
+        int expResult = 0;
+        int result = CropControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of buyLand method, of class CropControl.
+     */
+    @Test
+    public void testBuyLand() {
+        System.out.println("buyLand");
+        int landPrice = 20;
+        int acresToBuy = 10;
+        CropData _cropData = new CropData();
+        _cropData.setWheatInStore(1000);
+        _cropData.setAcresOwned(100);
+        int expResult = 110;
+        int result = CropControl.buyLand(landPrice, acresToBuy, _cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of feedPeople method, of class CropControl.
+     */
+    @Test
+    public void testFeedPeople() {
+        System.out.println("feedPeople");
+        int bushelsReservedForFood = 0;
+        CropData _cropData = null;
+        int expResult = 0;
+        int result = CropControl.feedPeople(bushelsReservedForFood, _cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
