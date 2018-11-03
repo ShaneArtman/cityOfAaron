aa7bd914e8c1e050ac5a969c3ae3256dc332a4a7
nrham <nrham@NX-T460p>
2018-11-02 22:56:19 -0700

    Updated getMenuView. Having issues merging with Pull from repository.

---
+++ cityOfAaron/src/control/GameControl.java
@@ -0,0 +1,36 @@
+/*
+ * GameControl.java
+ *
+ * The GameControl Class for the cityOfAaron project
+ * Created in the control package.
+ * 
+ * Created on Nov 3, 2018, 12:50:52 AM
+ * Created by artieman1
+ *
+ * CIT-260: 08
+ * Fall 2018
+ * Team members: Shane Artman, Nick Hammond, Jonathan Unga
+ *
+ * Purpose: <What is the purpose of the file?>
+ */
+
+package control;
+import model.*;
+import cityofaaron.CityOfAaron;
+
+
+public class GameControl {
+    
+    public static void createNewGame (String _name) {
+        // Create a new Game object.
+        // Create a new Player object
+        //  Store the name of the player in the Player object
+        // Store a reference to the Player object in the Game object
+        // Create a CropData object
+        // Initialize the data values in the CropData object
+        // Save a reference to the CropData object in the Game object
+        // Save a reference to the Game object in the static variable
+        // declared in the CityOfAaron class.
+    }
+
+}
---
+++ cityOfAaron/src/model/Map.java
@@ -0,0 +1,49 @@
+/*
+ * Map.java
+ *
+ * The Map Class for the cityOfAaron project
+ * Created in the model package.
+ * 
+ * Created on Nov 2, 2018, 11:48:30 PM
+ * Created by artieman1
+ *
+ * CIT-260: 08
+ * Fall 2018
+ * Team members: Shane Artman, Nick Hammond, Jonathan Unga
+ *
+ * Purpose: <What is the purpose of the file?>
+ */
+
+package model;
+
+
+public class Map {
+private int rowCount;
+private int colCount;
+private int[][] location;
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getColCount() {
+        return colCount;
+    }
+
+    public void setColCount(int colCount) {
+        this.colCount = colCount;
+    }
+
+    public int[][] getLocation() {
+        return location;
+    }
+
+    public void setLocation(int[][] location) {
+        this.location = location;
+    }
+
+}
